0077626e4ead2e1765c6924766db85168e9f7a3d
swaroski <bhise.suyog@gmail.com>
2022-12-27 16:35:36 -0500

    added comments

--- flame_thrower.py
+++ flame_thrower.py
@@ -2,15 +2,17 @@
 # coding: utf-8
 
 # In[11]:
-#converts the video with heatmap back to a video. It first executes the videos2frames.py to convert video to frames.
-#Then a heatmap is applied over those frames using heatmap_generator. Lastly, the frames with the heatmap are converted back to a video.  
+'''The code converts a video file to a series of frames and then applies a heatmap to each frame. The heatmap is generated 
+based on data from a CSV file. Finally, the frames with the heatmap are converted back to a video file, which is uploaded 
+to an S3 bucket. The code first imports several modules, including video2frames, Heatmap_on_Frames, and frame_to_video, 
+which are all custom modules. It also sets the locations of the input video file, the output frames, the heatmapped frames, 
+and the output video file. The generate_video function is defined and takes a histo_datafile as an argument. 
+Inside the function, the heatmap_generator function is called with the output_loc, heatmap_frames_dir, and histo_datafile 
+as arguments. The frame2video function is then called with the heatmap_frames_dir and output_video_name as arguments. 
+The output_video_name is then uploaded to an S3 bucket with the upload_file_to_bucket function.
+Finally, if the script is run as the main script, the generate_video function is called with histo_datafile as the argument.'''
 
 from video2frames import video2frames
-from write2csv import write2csv
-import cv2
-import csv
-import os
-import time
 import Heatmap_on_Frames as hf
 import frame_to_video as fv
 import utils.s3utils as s3
@@ -18,41 +20,23 @@ import utils.s3utils as s3
 input_loc = "./temp/downloads/demo.mp4"
 output_loc = "./temp/frames"
 heatmap_frames_dir = "./temp/heatmapped_frames"
-#histo_datafile = "./static/db/histo_eyes.csv"
-#histo_datafile = "./temp/data/b74830fb-12e2-4205-b150-f0d8c1b4ee07_eyes.csv"
-output_video_name="./temp/uploads/abc.avi"
-object_key="processed/abc.avi"
-BUCKET="focusai-private-sb"
+output_video_name = "./temp/uploads/abc.avi"
+object_key = "processed/abc.avi"
+BUCKET = "focusai-private-sb"
 
-
-#video2frames(input_loc)    
-
-#time.sleep(10)
-#hf.heatmap_generator(output_loc, heatmap_frames_dir, histo_datafile)
-#time.sleep(10)
-
-#fv.frame2video(heatmap_frames_dir, output_video_name)
-
-#s3.upload_file(f"processed/abc.avi", BUCKET)
-#s3.upload_file_to_bucket(output_video_name, f"processed/abc.avi", BUCKET)
-####write2csv()
 
 def generate_video(histo_datafile):
-    #Since we are using the same video for embedding the heatmap on, we don't need to do the 
-    #video2frame conversion everytime. This will save some processing time.
-    #video2frames(input_loc)
-    #time.sleep(10)
-    print("Generate video file ")
+    # Since we are using the same video for embedding the heatmap on, we don't need to do the 
+    # video2frame conversion every time. This will save some processing time.
+    video2frames(input_loc, output_loc)
     hf.heatmap_generator(output_loc, heatmap_frames_dir, histo_datafile)
-    #time.sleep(10)
     fv.frame2video(heatmap_frames_dir, output_video_name)
-    filename=os.path.basename(histo_datafile)
+    filename = os.path.basename(histo_datafile)
     print("Uploading eyegaze csv to S3: ", filename)
     s3.upload_file_to_bucket(output_video_name, filename, BUCKET)
-
     print("Video with heatmap uploaded to S3", output_video_name)
     s3.upload_file_to_bucket(output_video_name, object_key, BUCKET)
 
-
 if __name__ == "__main__":
     generate_video(histo_datafile)
+
